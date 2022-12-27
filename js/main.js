9b07724b2d2c7dcd2ded7d6b2192afceb627cca0
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-27 21:53:55 +0000

    added event listener to properly add in preview image relative to its input's absolute URL

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ function handleSubmit(event) {
   event.preventDefault();
 }
 $submit.addEventListener('submit', handleSubmit);
+
+var $photoUrl = document.querySelector('#photo');
+var $previewImage = document.querySelector('img');
+function handlePreview(event) {
+  event.preventDefault();
+  $previewImage.setAttribute('src', $photoUrl.value);
+}
+$photoUrl.addEventListener('input', handlePreview);
