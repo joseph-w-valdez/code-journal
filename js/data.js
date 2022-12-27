43006db539f025096520576619f8821dfb6bf393
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-27 22:45:12 +0000

    added beforeUnload event listener to the window to store the entry data to local storage

--- js/data.js
+++ js/data.js
@@ -6,3 +6,10 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', stringifyEntryValues);
+
+function stringifyEntryValues(object) {
+  var entryValuesString = JSON.stringify(data);
+  localStorage.setItem('entries-data', entryValuesString);
+}
