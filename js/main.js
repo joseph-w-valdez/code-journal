b372fae9ec040750d7cd3a098d3cce79a0a11142
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-28 00:35:31 +0000

    added function to render new DOM tree for each entry item

--- js/main.js
+++ js/main.js
@@ -24,3 +24,29 @@ function handlePreview(event) {
   $previewImage.setAttribute('src', $photoUrl.value);
 }
 $photoUrl.addEventListener('input', handlePreview);
+
+function renderEntry(entry) {
+  var newLi = document.createElement('li');
+  var newRow = document.createElement('div');
+  var newImg = document.createElement('img');
+  var newImgUrl = entry.photoUrl;
+  var newColumnFirstHalf = document.createElement('div');
+  var newColumnSecondHalf = document.createElement('div');
+  var newH2 = document.createElement('H2');
+  var newP = document.createElement('p');
+
+  newRow.setAttribute('class', 'row');
+  newLi.appendChild(newRow);
+  newColumnFirstHalf.setAttribute('class', 'column-half');
+  newImg.setAttribute('src', newImgUrl);
+  newColumnFirstHalf.appendChild(newImg);
+  newColumnSecondHalf.setAttribute('class', 'column-half');
+  newColumnSecondHalf.appendChild(newH2);
+  newColumnSecondHalf.appendChild(newP);
+  newRow.appendChild(newColumnFirstHalf);
+  newRow.appendChild(newColumnSecondHalf);
+
+  return newLi;
+}
+
+document.addEventListener('DOMContentLoaded', renderEntry);
