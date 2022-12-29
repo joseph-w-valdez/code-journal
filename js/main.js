8697f5ec58573893c89e7c6b1cd602be49e1960d
Joseph Valdez <Joseph.W.Valdez@Gmail.com>
2022-12-29 00:32:37 +0000

    updated submit handler to check if data.editing is null

--- js/main.js
+++ js/main.js
@@ -3,19 +3,20 @@ var $form = document.querySelector('form');
 function handleSubmit(event) {
   var entry = {};
   event.preventDefault();
-  entry.title = $title.value;
-  entry.photoUrl = $photoUrl.value;
-  entry.notes = $notes.value;
-  entry.entryId = data.nextEntryId;
-  data.nextEntryId = data.nextEntryId + 1;
-  data.entries.unshift(entry);
-  $previewImage.setAttribute('src', './images/placeholder-image-square.jpg');
-  $form.reset();
-  var $singleEntry = renderEntry(entry);
-  $ul.prepend($singleEntry);
-  viewSwap('entries');
-  toggleNoEntries();
-
+  if (!data.editing) {
+    entry.title = $title.value;
+    entry.photoUrl = $photoUrl.value;
+    entry.notes = $notes.value;
+    entry.entryId = data.nextEntryId;
+    data.nextEntryId = data.nextEntryId + 1;
+    data.entries.unshift(entry);
+    $previewImage.setAttribute('src', './images/placeholder-image-square.jpg');
+    $form.reset();
+    var $singleEntry = renderEntry(entry);
+    $ul.prepend($singleEntry);
+    viewSwap('entries');
+    toggleNoEntries();
+  }
 }
 
 $form.addEventListener('submit', handleSubmit);
